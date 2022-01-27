b681681b794f8cdaa2fec9d51090291eb2ab3dce
Alex Dean <alex@zanconsulting.com>
2022-01-27 13:51:23 -0700

    ZanArray - simplify createFromString since there are now better methods for handling what it was originally written for

--- src/Util/ZanArray.php
+++ src/Util/ZanArray.php
@@ -7,79 +7,21 @@ namespace Zan\CommonBundle\Util;
 class ZanArray
 {
     /**
-     * Attempts to parse a string and split by the best delimiter.
-     * If the "=" sign is present, it is used as a key/value delimiter
-     * and a hash is returned.  All values are trimmed before they are
-     * returned.  Any empty values are not added to the array (unless they have
-     * a key)
+     * Similar to explode with support for handling things that may already be arrays or may be empty
      *
-     * Supported delimiters:
-     * \verbatim ; , & \endverbatim
-     *
-     * This is a port of the legacy ZanUtils::smartSplit method
-     *
-     * @param mixed $str The string to split
-     * @param string $forceDelimiter
-     * @param string $forceValueDelimiter
-     *
-     * @return array An array or hash
+     * @param null|string|array<mixed> $str
+     * @param non-empty-string $separator
+     * @return array<string>
      */
-    public static function createFromString($str, $forceDelimiter = ";", $forceValueDelimiter = "=")
+    public static function createFromString(null|string|array $str, string $separator = ","): array
     {
         // Special case: already an array
+        // @phpstan-ignore-next-line
         if (is_array($str)) return $str;
+
         // Special case: empty value
-        if (!$str) return array();
+        if (!$str) return [];
 
-        // Search the string for a possible delimiter to use
-        $delim = $forceDelimiter;
-        if (false === strpos($str, $delim)) $delim = ",";
-        if (false === strpos($str, $delim)) $delim = "&";
-
-        // Detect if there's a value delimiter or if it's just a basic array
-        $valueDelim = $forceValueDelimiter;
-        $hasValueDelim = false;
-        if (false !== strpos($str, $valueDelim)) $hasValueDelim = true;
-
-        // If we're on our final delimiter and it's still not in the string, assume that this
-        // is a single item string
-        if (false === strpos($str, $delim)) {
-            if ($hasValueDelim) {
-                $parts = explode($valueDelim, $str);
-                return array (trim($parts[0]) => trim($parts[1]));
-            }
-            else {
-                return array($str);
-            }
-        }
-
-        // If our delimiter is a &, we assume it's a url encoded string
-        if ($delim == "&") {
-            $parsedValues = array();
-            parse_str($str, $parsedValues);
-            return $parsedValues;
-        }
-        else {
-            // Split by the detected delimiter
-            $split = explode($delim, $str);
-
-            $finVals = array();
-            foreach ($split as $item) {
-                $item = trim($item);
-                if ($item == "") continue;
-
-                // If we have equals, create a key in the array
-                if ($hasValueDelim) {
-                    list($key, $value) = explode($valueDelim, $item);
-                    $key = trim($key);
-                    $value = trim($value);
-                    $finVals[$key] = $value;
-                } else {
-                    $finVals[] = $item;
-                }
-            }
-
-            return $finVals;
-        }
+        return explode($separator, $str);
     }
 }
